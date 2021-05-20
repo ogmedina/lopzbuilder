5df4a8d3d7eea10e223eea3d525c6ff4c36af2e4
Oscar Medina <ogmedina04@gmail.com>
2021-05-20 15:22:29 -0500

    styling main logo

--- src/Components/Jumbotron/index.js
+++ src/Components/Jumbotron/index.js
@@ -1,11 +1,18 @@
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
+import logo from './images/logo2.png'
+import Container from 'react-bootstrap/Container';
 
 function JumboTron() {
     return(
         <>        
         <Jumbotron className="jumbotron">
+        <Container>
+            
+                <img className="jumboLogo" src={logo} alt="logo" />
+            
         
+        </Container>
         <div className="jumbotron-heading text-uppercase text-center"></div>    
             
         {/* <p>
